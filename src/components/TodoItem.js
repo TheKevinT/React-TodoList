87a0b1025052fe12238cd31283d71be52d0a071a
thekevinT <kevinftipantiza@gmail.com>
2020-05-01 12:22:42 -0500

    creando archivos

---
+++ src/components/TodoItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TodoItem extends Component {
+    render() {
+        return (
+            <div>
+                hola desde TODO ITEM
+            </div>
+        )
+    }
+}
